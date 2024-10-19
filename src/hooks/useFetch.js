ce0594e1e968ffc5e5542ac04023a400e4885e13
Adam Colvin <adamcolv@icloud.com>
2024-10-19 17:02:06 +0100

    Add API Key

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 export function authenticatedFetch(uri) {
-  const token = "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"
   return fetch(uri, {
     method: "GET",
     headers: {
-      'Authorization': `Bearer ${token}`,
-      'Accept': 'application/vnd.github.v3+json'
-    }
+      Authorization: `Bearer ${process.env.API_KEY}`,
+      Accept: "application/vnd.github.v3+json",
+    },
   });
 }
 
